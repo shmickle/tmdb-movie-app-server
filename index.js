40f80edc9babc7cb8b0eabfe07654c45c4343736
unknown <eric.bryant0@gmail.com>
2020-08-06 19:16:58 -0400

    added routes

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const routes = require('./routes')
+
+app.set('port', PORT)
+app.set('env', NODE_ENV)
+app.use(logger('tiny'))
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.use('/', routes)
+
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} Not Found`)
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      message: err.message,
+    },
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(
+    `Express Server started on Port ${app.get(
+      'port'
+    )} | Environment : ${app.get('env')}`
+  )
+})
---
+++ routes/index.js
@@ -0,0 +1,54 @@
+const routes = require('express').Router()
+const axios = require('axios')
+const apiKey = '?api_key=' + require('../api')
+
+const apiClient = axios.create({
+  baseURL: `https://api.themoviedb.org/3/`,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+routes.get('/search/multi/:query', (req, res) => {
+  const query = req.params.query
+
+  apiClient.get(`/search/multi${apiKey}&query=${query}`).then((result) => {
+    res.status(200).send(result.data)
+  })
+})
+
+routes.get('/search/person/:query', (req, res) => {
+  const query = req.params.query
+
+  apiClient.get(`/search/person${apiKey}&query=${query}`).then((result) => {
+    res.status(200).send(result.data)
+  })
+})
+
+routes.get('/search/movie/:query', (req, res) => {
+  const query = req.params.query
+
+  apiClient.get(`/search/movie${apiKey}&query=${query}`).then((result) => {
+    res.status(200).send(result.data)
+  })
+})
+
+routes.get('/search/tv/:query', (req, res) => {
+  const query = req.params.query
+
+  apiClient.get(`/search/tv${apiKey}&query=${query}`).then((result) => {
+    res.status(200).send(result.data)
+  })
+})
+
+routes.get('/movie/:id', (req, res) => {
+  const movieID = req.params.id
+
+  apiClient.get(`/movie/${movieID}${apiKey}`).then((result) => {
+    res.status(200).send(result.data)
+  })
+})
+
+module.exports = routes
