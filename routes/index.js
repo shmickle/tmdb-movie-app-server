8ae02a6ba480ac94ccc955559e16fd12b72f0422
unknown <eric.bryant0@gmail.com>
2020-08-15 16:50:05 -0400

    error handling on routes

--- routes/index.js
+++ routes/index.js
@@ -14,41 +14,66 @@ const apiClient = axios.create({
 routes.get('/search/multi/:query', (req, res) => {
   const query = req.params.query
 
-  apiClient.get(`/search/multi${apiKey}&query=${query}`).then((result) => {
-    res.status(200).send(result.data)
-  })
+  apiClient
+    .get(`/search/multi${apiKey}&query=${query}`)
+    .then((result) => {
+      res.status(200).send(result.data)
+    })
+    .catch((error) => {
+      res.status(404).send(error)
+    })
 })
 
 routes.get('/search/person/:query', (req, res) => {
   const query = req.params.query
 
-  apiClient.get(`/search/person${apiKey}&query=${query}`).then((result) => {
-    res.status(200).send(result.data)
-  })
+  apiClient
+    .get(`/search/person${apiKey}&query=${query}`)
+    .then((result) => {
+      res.status(200).send(result.data)
+    })
+    .catch((error) => {
+      res.status(404).send(error)
+    })
 })
 
 routes.get('/search/movie/:query', (req, res) => {
   const query = req.params.query
 
-  apiClient.get(`/search/movie${apiKey}&query=${query}`).then((result) => {
-    res.status(200).send(result.data)
-  })
+  apiClient
+    .get(`/search/movie${apiKey}&query=${query}`)
+    .then((result) => {
+      res.status(200).send(result.data)
+    })
+    .catch((error) => {
+      res.status(404).send(error)
+    })
 })
 
 routes.get('/search/tv/:query', (req, res) => {
   const query = req.params.query
 
-  apiClient.get(`/search/tv${apiKey}&query=${query}`).then((result) => {
-    res.status(200).send(result.data)
-  })
+  apiClient
+    .get(`/search/tv${apiKey}&query=${query}`)
+    .then((result) => {
+      res.status(200).send(result.data)
+    })
+    .catch((error) => {
+      res.status(404).send(error)
+    })
 })
 
 routes.get('/movie/:id', (req, res) => {
   const movieID = req.params.id
 
-  apiClient.get(`/movie/${movieID}${apiKey}`).then((result) => {
-    res.status(200).send(result.data)
-  })
+  apiClient
+    .get(`/movie/${movieID}${apiKey}`)
+    .then((result) => {
+      res.status(200).send(result.data)
+    })
+    .catch((error) => {
+      res.status(404).send(error)
+    })
 })
 
 module.exports = routes
